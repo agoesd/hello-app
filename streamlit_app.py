0da10a7e25c2e77e0ea293923df9f807d834f6ee
agoesd <85086014+agoesd@users.noreply.github.com>
2023-07-01 16:06:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import joblib
-import numpy as np
 
 # Set page title and layout
 st.set_page_config(
@@ -17,16 +16,13 @@ st.sidebar.markdown("Enter the input features below:")
 # Create input fields for features
 TglPengumuman_Bln = st.sidebar.selectbox("Bulan Pengumuman", range(1, 13), index=0)
 Lokasi_enc = st.sidebar.selectbox("Lokasi Pekerjaan", range(0, 37), index=0)
-Klasifikasi_enc = st.sidebar.selectbox("Klasifikasi Barang/Jasa", ["Jasa Konsultansi", "Jasa Lainnya", "Pekerjaan Konstruksi", "Pengadaan Barang"], index=0)
-
-# Map Klasifikasi_enc to corresponding feature values
-KlasBJ_JasaKonsultasi = 1 if Klasifikasi_enc == "Jasa Konsultansi" else 0
-KlasBJ_JasaLainnya = 1 if Klasifikasi_enc == "Jasa Lainnya" else 0
-KlasBJ_PekerjaanKonstruksi = 1 if Klasifikasi_enc == "Pekerjaan Konstruksi" else 0
-KlasBJ_PengadaanBarang = 1 if Klasifikasi_enc == "Pengadaan Barang" else 0
-
+Klasifikasi_enc = st.sidebar.selectbox("Klasifikasi", [0, 1, 3], index=0)
 JenisBelanja_enc = st.sidebar.selectbox("Jenis Belanja", [0, 1], index=0)
 MetodePengadaan_enc = st.sidebar.selectbox("Metode Pengadaan", [0, 1], index=0)
+KlasBJ_JasaKonsultasi = st.sidebar.selectbox("Jasa Konsultasi?", [0, 1], index=0)
+KlasBJ_JasaLainnya = st.sidebar.selectbox("Jasa Lainnya?", [0, 1], index=0)
+KlasBJ_PekerjaanKonstruksi = st.sidebar.selectbox("Pekerjaan Konstruksi?", [0, 1], index=0)
+KlasBJ_PengadaanBarang = st.sidebar.selectbox("Pengadaan Barang?", [0, 1], index=0)
 Pagu2 = st.sidebar.number_input("Nilai Pagu")
 HPS2 = st.sidebar.number_input("Nilai HPS")
 
@@ -42,20 +38,11 @@ st.markdown("---")
 
 # Make predictions button
 if st.button("Make Predictions"):
-    # Prepare input data for prediction
-    input_data = np.array([[TglPengumuman_Bln, Lokasi_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, JenisBelanja_enc, MetodePengadaan_enc, Pagu2, HPS2]])
-
-    # Reshape input data
-    input_data = np.reshape(input_data, (1, -1))
-
-    # Ensure the input data is of the correct data type
-    input_data = input_data.astype(float)
-
     # Make Procurement Time prediction
-    time_prediction = time_model.predict(input_data.reshape(1, -1))[0]
-    
+    time_prediction = time_model.predict([[TglPengumuman_Bln, Lokasi_enc, Klasifikasi_enc, JenisBelanja_enc, MetodePengadaan_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, Pagu2, HPS2]])[0]
+
     # Make Efisiensi prediction
-    efisiensi_prediction = efisiensi_model.predict(input_data.reshape(1, -1))[0]
+    efisiensi_prediction = efisiensi_model.predict([[TglPengumuman_Bln, Lokasi_enc, Klasifikasi_enc, JenisBelanja_enc, MetodePengadaan_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, Pagu2, HPS2]])[0]
 
     # Display predictions
     st.success(f"Predicted Procurement Time: {time_prediction}")
