592efae612e1162ab6f56312ca34d425879c81f4
agoesd <85086014+agoesd@users.noreply.github.com>
2023-07-01 15:26:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import pandas as pd
-from sklearn.metrics import mean_squared_error, mean_absolute_error
 import joblib
 
-# Set page title
-st.set_page_config(page_title="Procurement Time Prediction App", layout="wide")
+# Set page title and layout
+st.set_page_config(
+    page_title="Procurement Time Prediction App",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
-# Set sidebar
+# Set sidebar title and description
 st.sidebar.title("Procurement Time Prediction")
+st.sidebar.markdown("Enter the input features below:")
 
 # Create input fields for features
 TglPengumuman_Bln = st.sidebar.number_input("Bulan Pengumuman")
@@ -16,21 +20,23 @@ Klasifikasi_enc = st.sidebar.number_input("Klasifikasi")
 JenisBelanja_enc = st.sidebar.number_input("Jenis Belanja")
 MetodePengadaan_enc = st.sidebar.number_input("Metode Pengadaan")
 KlasBJ_JasaKonsultasi = st.sidebar.number_input("Jasa Konsultasi?")
-KlasBJ_JasaLainnya  = st.sidebar.number_input("Jasa Lainnya?")
+KlasBJ_JasaLainnya = st.sidebar.number_input("Jasa Lainnya?")
 KlasBJ_PekerjaanKonstruksi = st.sidebar.number_input("Pekerjaan Konstruksi?")
 KlasBJ_PengadaanBarang = st.sidebar.number_input("Pengadaan Barang?")
 Pagu2 = st.sidebar.number_input("Nilai Pagu")
 HPS2 = st.sidebar.number_input("Nilai HPS")
-# Add more feature inputs if needed
 
 # Load the joblib model
 model = joblib.load("modeldecisiontree_lamatender.joblib")
 
 # Procurement time prediction
-st.subheader("Procurement Time Prediction")
+st.title("Procurement Time Prediction")
+st.markdown("---")
 
-# Make prediction
-prediction = model.predict([[TglPengumuman_Bln, Lokasi_enc, Klasifikasi_enc, JenisBelanja_enc, MetodePengadaan_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, Pagu2, HPS2]])[0]
+# Make prediction button
+if st.button("Predict"):
+    # Make prediction
+    prediction = model.predict([[TglPengumuman_Bln, Lokasi_enc, Klasifikasi_enc, JenisBelanja_enc, MetodePengadaan_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, Pagu2, HPS2]])[0]
 
-# Display prediction
-st.write("Predicted Procurement Time:", prediction)
+    # Display prediction
+    st.success(f"Predicted Procurement Time: {prediction}")
