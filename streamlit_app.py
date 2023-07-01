fb0ac80152a94319842b4a093b0d4633d38478d7
agoesd <85086014+agoesd@users.noreply.github.com>
2023-07-01 17:05:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,24 +14,42 @@ st.sidebar.title("Procurement Prediction")
 st.sidebar.markdown("Enter the input features below:")
 
 # Create input fields for features
-bulan_options = {
-    "Januari": 0,
-    "Februari": 1,
-    "Maret": 2,
-    "April": 3,
-    "Mei": 4,
-    "Juni": 5,
-    "Juli": 6,
-    "Agustus": 7,
-    "September": 8,
-    "Oktober": 9,
-    "November": 10,
-    "Desember": 11
+lokasi_options = {
+    "Aceh": 23,
+    "Bali": 13,
+    "Banten": 5,
+    "Bengkulu": 25,
+    "DI Yogyakarta": 9,
+    "DKI Jakarta": 0,
+    "Jambi": 7,
+    "Jawa Barat": 8,
+    "Jawa Tengah": 11,
+    "Jawa Timur": 1,
+    "Kalimantan Selatan": 21,
+    "Kalimantan Tengah": 16,
+    "Kalimantan Timur": 2,
+    "Kalimantan Utara": 19,
+    "Kepulauan Riau": 4,
+    "Lampung": 17,
+    "Maluku": 27,
+    "Nusa Tenggara Barat": 26,
+    "Nusa Tenggara Timur": 15,
+    "Papua": 24,
+    "Papua Barat": 29,
+    "Riau": 12,
+    "Sulawesi Barat": 28,
+    "Sulawesi Selatan": 3,
+    "Sulawesi Tengah": 6,
+    "Sulawesi Tenggara": 20,
+    "Sulawesi Utara": 22,
+    "Sumatera Barat": 18,
+    "Sumatera Selatan": 14,
+    "Sumatera Utara": 10
 }
-TglPengumuman_Bln = st.sidebar.selectbox("Bulan Pengumuman", list(bulan_options.keys()), index=0)
-TglPengumuman_Bln_value = bulan_options[TglPengumuman_Bln]
 
-Lokasi_enc = st.sidebar.selectbox("Lokasi Pekerjaan", range(0, 37), index=0)
+Lokasi_enc = st.sidebar.selectbox("Lokasi Pekerjaan", list(lokasi_options.keys()), index=0)
+Lokasi_enc_value = lokasi_options[Lokasi_enc]
+
 Klasifikasi_enc = st.sidebar.selectbox("Klasifikasi", [0, 1, 3], index=0)
 JenisBelanja_enc = st.sidebar.selectbox("Jenis Belanja", [0, 1], index=0)
 MetodePengadaan_enc = st.sidebar.selectbox("Metode Pengadaan", [0, 1], index=0)
@@ -56,8 +74,7 @@ st.markdown("---")
 if st.button("Make Predictions"):
     # Prepare input data for prediction
     input_data = pd.DataFrame({
-        "TglPengumuman_Bln": [TglPengumuman_Bln_value],
-        "Lokasi_enc": [Lokasi_enc],
+        "Lokasi_enc": [Lokasi_enc_value],
         "Klasifikasi_enc": [Klasifikasi_enc],
         "JenisBelanja_enc": [JenisBelanja_enc],
         "MetodePengadaan_enc": [MetodePengadaan_enc],
