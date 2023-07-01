59e00d4bae5e2db92f69cd0780bb8c9f057c6fbe
agoesd <85086014+agoesd@users.noreply.github.com>
2023-07-01 15:23:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-import pickle
+import joblib
 
 # Set page title
 st.set_page_config(page_title="Procurement Time Prediction App", layout="wide")
@@ -23,15 +23,14 @@ Pagu2 = st.sidebar.number_input("Nilai Pagu")
 HPS2 = st.sidebar.number_input("Nilai HPS")
 # Add more feature inputs if needed
 
-# Load the pickle model
-with open("modeldecisiontree_lamatender.joblib", "rb") as file:
-    model = pickle.load(file)
+# Load the joblib model
+model = joblib.load("modeldecisiontree_lamatender.joblib")
 
 # Procurement time prediction
 st.subheader("Procurement Time Prediction")
 
 # Make prediction
-prediction = model.predict([[TglPengumuman_Bln, Lokasi_enc,Klasifikasi_enc,JenisBelanja_enc,MetodePengadaan_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, Pagu2, HPS2]])[0]
+prediction = model.predict([[TglPengumuman_Bln, Lokasi_enc, Klasifikasi_enc, JenisBelanja_enc, MetodePengadaan_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, Pagu2, HPS2]])[0]
 
 # Display prediction
 st.write("Predicted Procurement Time:", prediction)
