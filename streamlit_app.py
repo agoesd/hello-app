2b7b5e6f24f70210e43bdbcf234dd3514665f924
agoesd <85086014+agoesd@users.noreply.github.com>
2023-07-01 17:35:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,33 +31,60 @@ bulan_options = {
 TglPengumuman_Bln = st.sidebar.selectbox("Bulan Pengumuman", list(bulan_options.keys()), index=0)
 TglPengumuman_Bln_value = bulan_options[TglPengumuman_Bln]
 
-Lokasi_enc = st.sidebar.selectbox("Lokasi Pekerjaan", range(0, 37), index=0)
+lokasi_options = {
+    "Aceh": 23,
+    "Bali": 13,
+    "Banten": 5,
+    "Bengkulu": 25,
+    "DI Yogyakarta": 9,
+    "DKI Jakarta": 0,
+    "Jambi": 7,
+    "Jawa Barat": 8,
+    "Jawa Tengah": 11,
+    "Jawa Timur": 1,
+    "Kalimantan Selatan": 21,
+    "Kalimantan Tengah": 16,
+    "Kalimantan Timur": 2,
+    "Kalimantan Utara": 19,
+    "Kepulauan Riau": 4,
+    "Lampung": 17,
+    "Maluku": 27,
+    "Nusa Tenggara Barat": 26,
+    "Nusa Tenggara Timur": 15,
+    "Papua": 24,
+    "Papua Barat": 29,
+    "Riau": 12,
+    "Sulawesi Barat": 28,
+    "Sulawesi Selatan": 3,
+    "Sulawesi Tengah": 6,
+    "Sulawesi Tenggara": 20,
+    "Sulawesi Utara": 22,
+    "Sumatera Barat": 18,
+    "Sumatera Selatan": 14,
+    "Sumatera Utara": 10
+}
+Lokasi_enc = st.sidebar.selectbox("Lokasi Pekerjaan", list(lokasi_options.keys()), index=0)
+Lokasi_enc_value = lokasi_options[Lokasi_enc]
+
 Klasifikasi_enc = st.sidebar.selectbox("Klasifikasi", [0, 1, 3], index=0)
 JenisBelanja_enc = st.sidebar.selectbox("Jenis Belanja", [0, 1], index=0)
-MetodePengadaan_enc = st.sidebar.selectbox("Metode Pengadaan", [0, 1], index=0)
+MetodePengadaan_enc = st.sidebar.selectbox("Metode Pengadaan", ["Tender", "Seleksi"], index=0)
+MetodePengadaan_enc_value = 1 if MetodePengadaan_enc == "Tender" else 0
 
-jenis_pengadaan = st.sidebar.selectbox("Jenis Pengadaan", ["Jasa Konsultasi", "Jasa Lainnya", "Pekerjaan Konstruksi", "Pengadaan Barang"], index=0)
+# Create input field for Jenis Pengadaan
+jenis_pengadaan_options = {
+    "Jasa Konsultasi": "KlasBJ_JasaKonsultasi",
+    "Jasa Lainnya": "KlasBJ_JasaLainnya",
+    "Pekerjaan Konstruksi": "KlasBJ_PekerjaanKonstruksi",
+    "Pengadaan Barang": "KlasBJ_PengadaanBarang"
+}
+jenis_pengadaan = st.sidebar.selectbox("Jenis Pengadaan", list(jenis_pengadaan_options.keys()), index=0)
 
-if jenis_pengadaan == "Jasa Konsultasi":
-    KlasBJ_JasaKonsultasi = 1
-    KlasBJ_JasaLainnya = 0
-    KlasBJ_PekerjaanKonstruksi = 0
-    KlasBJ_PengadaanBarang = 0
-elif jenis_pengadaan == "Jasa Lainnya":
-    KlasBJ_JasaKonsultasi = 0
-    KlasBJ_JasaLainnya = 1
-    KlasBJ_PekerjaanKonstruksi = 0
-    KlasBJ_PengadaanBarang = 0
-elif jenis_pengadaan == "Pekerjaan Konstruksi":
-    KlasBJ_JasaKonsultasi = 0
-    KlasBJ_JasaLainnya = 0
-    KlasBJ_PekerjaanKonstruksi = 1
-    KlasBJ_PengadaanBarang = 0
-else:
-    KlasBJ_JasaKonsultasi = 0
-    KlasBJ_JasaLainnya = 0
-    KlasBJ_PekerjaanKonstruksi = 0
-    KlasBJ_PengadaanBarang = 1
+# Set the value of the selected jenis pengadaan
+KlasBJ_JasaKonsultasi = 1 if jenis_pengadaan_options[jenis_pengadaan] == "KlasBJ_JasaKonsultasi" else 0
+KlasBJ_JasaLainnya = 1 if jenis_pengadaan_options[jenis_pengadaan] == "KlasBJ_JasaLainnya" else 0
+KlasBJ_PekerjaanKonstruksi = 1 if jenis_pengadaan_options[jenis_pengadaan] == "KlasBJ_PekerjaanKonstruksi" else 0
+KlasBJ_PengadaanBarang = 1 if jenis_pengadaan_options[jenis_pengadaan] == "KlasBJ_PengadaanBarang" else 0
 
 Pagu2 = st.sidebar.number_input("Nilai Pagu")
 HPS2 = st.sidebar.number_input("Nilai HPS")
@@ -77,10 +104,10 @@ if st.button("Make Predictions"):
     # Prepare input data for prediction
     input_data = pd.DataFrame({
         "TglPengumuman_Bln": [TglPengumuman_Bln_value],
-        "Lokasi_enc": [Lokasi_enc],
+        "Lokasi_enc": [Lokasi_enc_value],
         "Klasifikasi_enc": [Klasifikasi_enc],
         "JenisBelanja_enc": [JenisBelanja_enc],
-        "MetodePengadaan_enc": [MetodePengadaan_enc],
+        "MetodePengadaan_enc": [MetodePengadaan_enc_value],
         "KlasBJ_JasaKonsultasi": [KlasBJ_JasaKonsultasi],
         "KlasBJ_JasaLainnya": [KlasBJ_JasaLainnya],
         "KlasBJ_PekerjaanKonstruksi": [KlasBJ_PekerjaanKonstruksi],
