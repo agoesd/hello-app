4aaf7b9eaa7daedb7ff0403981feb053edcbfc73
agoesd <85086014+agoesd@users.noreply.github.com>
2023-07-01 16:33:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import pandas as pd
 import joblib
+import pandas as pd
 
 # Set page title and layout
 st.set_page_config(
@@ -14,7 +14,6 @@ st.sidebar.title("Procurement Prediction")
 st.sidebar.markdown("Enter the input features below:")
 
 # Create input fields for features
-#TglPengumuman_Bln = st.sidebar.selectbox("Bulan Pengumuman", range(1, 13), index=0)
 bulan_options = {
     "Januari": 0,
     "Februari": 1,
@@ -30,7 +29,6 @@ bulan_options = {
     "Desember": 11
 }
 TglPengumuman_Bln = st.sidebar.selectbox("Bulan Pengumuman", list(bulan_options.keys()), index=0)
-# Get the corresponding value based on the selected month
 TglPengumuman_Bln_value = bulan_options[TglPengumuman_Bln]
 
 Lokasi_enc = st.sidebar.selectbox("Lokasi Pekerjaan", range(0, 37), index=0)
@@ -56,11 +54,26 @@ st.markdown("---")
 
 # Make predictions button
 if st.button("Make Predictions"):
+    # Prepare input data for prediction
+    input_data = pd.DataFrame({
+        "TglPengumuman_Bln": [TglPengumuman_Bln_value],
+        "Lokasi_enc": [Lokasi_enc],
+        "Klasifikasi_enc": [Klasifikasi_enc],
+        "JenisBelanja_enc": [JenisBelanja_enc],
+        "MetodePengadaan_enc": [MetodePengadaan_enc],
+        "KlasBJ_JasaKonsultasi": [KlasBJ_JasaKonsultasi],
+        "KlasBJ_JasaLainnya": [KlasBJ_JasaLainnya],
+        "KlasBJ_PekerjaanKonstruksi": [KlasBJ_PekerjaanKonstruksi],
+        "KlasBJ_PengadaanBarang": [KlasBJ_PengadaanBarang],
+        "Pagu2": [Pagu2],
+        "HPS2": [HPS2]
+    })
+
     # Make Procurement Time prediction
-    time_prediction = time_model.predict([[TglPengumuman_Bln_value, Lokasi_enc, Klasifikasi_enc, JenisBelanja_enc, MetodePengadaan_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, Pagu2, HPS2]])[0]
+    time_prediction = time_model.predict(input_data)[0]
 
     # Make Efisiensi prediction
-    efisiensi_prediction = efisiensi_model.predict([[TglPengumuman_Bln_value, Lokasi_enc, Klasifikasi_enc, JenisBelanja_enc, MetodePengadaan_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, Pagu2, HPS2]])[0]
+    efisiensi_prediction = efisiensi_model.predict(input_data)[0]
 
     # Display predictions
     st.success(f"Predicted Procurement Time: {time_prediction}")
