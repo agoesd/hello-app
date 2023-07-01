2da174cb27ef17cec007e7b1f8630328c63b33e8
agoesd <85086014+agoesd@users.noreply.github.com>
2023-07-01 15:54:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,9 @@ if st.button("Make Predictions"):
     # Prepare input data for prediction
     input_data = np.array([[TglPengumuman_Bln, Lokasi_enc, Klasifikasi_enc, JenisBelanja_enc, MetodePengadaan_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, Pagu2, HPS2]])
 
+    # Ensure the input data is of the correct data type
+    input_data = input_data.astype(float)
+
     # Make Procurement Time prediction
     time_prediction = time_model.predict(input_data)[0]
 
