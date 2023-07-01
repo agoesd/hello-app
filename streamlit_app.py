7505150c73eee6d199847095a066a781ddbc8e8e
agoesd <85086014+agoesd@users.noreply.github.com>
2023-07-01 15:14:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,17 @@ st.set_page_config(page_title="Procurement Time Prediction App", layout="wide")
 st.sidebar.title("Procurement Time Prediction")
 
 # Create input fields for features
-feature1 = st.sidebar.number_input("Feature 1")
-feature2 = st.sidebar.number_input("Feature 2")
+TglPengumuman_Bln = st.sidebar.number_input("Bulan Pengumuman")
+Lokasi_enc = st.sidebar.number_input("Lokasi Pekerjaan")
+Klasifikasi_enc = st.sidebar.number_input("Klasifikasi")
+JenisBelanja_enc = st.sidebar.number_input("Jenis Belanja")
+MetodePengadaan_enc = st.sidebar.number_input("Metode Pengadaan")
+KlasBJ_JasaKonsultasi = st.sidebar.number_input("Jasa Konsultasi?")
+KlasBJ_JasaLainnya  = st.sidebar.number_input("Jasa Lainnya?")
+KlasBJ_PekerjaanKonstruksi = st.sidebar.number_input("Pekerjaan Konstruksi?")
+KlasBJ_PengadaanBarang = st.sidebar.number_input("Pengadaan Barang?")
+Pagu2 = st.sidebar.number_input("Nilai Pagu")
+HPS2 = st.sidebar.number_input("Nilai HPS")
 # Add more feature inputs if needed
 
 # Load the pickle model
