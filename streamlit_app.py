588732237c2a55b1cb13d50bb7961ec42e7c3d4a
agoesd <85086014+agoesd@users.noreply.github.com>
2023-07-01 15:31:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,19 @@
+Sure! Here's the updated code with the file name changed to "modeldecisiontree_efisiensi.joblib":
+
+```python
 import streamlit as st
 import pandas as pd
 import joblib
 
 # Set page title and layout
 st.set_page_config(
-    page_title="Procurement Time Prediction App",
+    page_title="Procurement Prediction App",
     layout="wide",
     initial_sidebar_state="expanded"
 )
 
 # Set sidebar title and description
-st.sidebar.title("Procurement Time Prediction")
+st.sidebar.title("Procurement Prediction")
 st.sidebar.markdown("Enter the input features below:")
 
 # Create input fields for features
@@ -26,17 +29,29 @@ KlasBJ_PengadaanBarang = st.sidebar.number_input("Pengadaan Barang?")
 Pagu2 = st.sidebar.number_input("Nilai Pagu")
 HPS2 = st.sidebar.number_input("Nilai HPS")
 
-# Load the joblib model
-model = joblib.load("modeldecisiontree_lamatender.joblib")
+# Load the Procurement Time model
+time_model = joblib.load("modeldecisiontree_lamatender.joblib")
+
+# Load the Efisiensi model
+efisiensi_model = joblib.load("modeldecisiontree_efisiensi.joblib")
 
 # Procurement time prediction
-st.title("Procurement Time Prediction")
+st.title("Procurement Prediction")
 st.markdown("---")
 
-# Make prediction button
-if st.button("Predict"):
-    # Make prediction
-    prediction = model.predict([[TglPengumuman_Bln, Lokasi_enc, Klasifikasi_enc, JenisBelanja_enc, MetodePengadaan_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, Pagu2, HPS2]])[0]
+# Make predictions button
+if st.button("Make Predictions"):
+    # Make Procurement Time prediction
+    time_prediction = time_model.predict([[TglPengumuman_Bln, Lokasi_enc, Klasifikasi_enc, JenisBelanja_enc, MetodePengadaan_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, Pagu2, HPS2]])[0]
 
-    # Display prediction
-    st.success(f"Predicted Procurement Time: {prediction}")
+    # Make Efisiensi prediction
+    efisiensi_prediction = efisiensi_model.predict([[TglPengumuman_Bln, Lokasi_enc, Klasifikasi_enc, JenisBelanja_enc, MetodePengadaan_enc, KlasBJ_JasaKonsultasi, KlasBJ_JasaLainnya, KlasBJ_PekerjaanKonstruksi, KlasBJ_PengadaanBarang, Pagu2, HPS2]])[0]
+
+    # Display predictions
+    st.success(f"Predicted Procurement Time: {time_prediction}")
+    st.success(f"Predicted Efisiensi: {efisiensi_prediction}")
+```
+
+Make sure to replace "modeldecisiontree_efisiensi.joblib" with the correct file name of your "Efisiensi" model.
+
+Let me know if you have any further questions!
